194f79a5d46cfdc0da12e5791f301d0b5ae64147
charangoudchinthapalli@gmail.com <charangoudchinthapalli@gmail.com>
2025-05-22 11:05:06 +0530

    intial commit

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running at ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+   console.log("monogodb connection failed !!",err);
+   
+})
